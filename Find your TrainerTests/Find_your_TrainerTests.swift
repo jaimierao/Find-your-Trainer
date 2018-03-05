60372ee52c95da9af9e4adc22415b36409e93357
Jordan Rao <raojordan163@gmail.com>
2018-03-05 09:51:53 -0800

    Initial Commit

---
+++ Find your TrainerTests/Find_your_TrainerTests.swift
@@ -0,0 +1,36 @@
+//
+//  Find_your_TrainerTests.swift
+//  Find your TrainerTests
+//
+//  Created by Jaimie Rao on 2018-03-05.
+//  Copyright © 2018 Harvard Westlake. All rights reserved.
+//
+
+import XCTest
+@testable import Find_your_Trainer
+
+class Find_your_TrainerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
